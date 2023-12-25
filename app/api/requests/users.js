1a2ea1e7ff40314480fd5109292682db80330ab9
jajorgbor <joshuaajogbor@gmail.com>
2023-12-25 05:45:48 +0100

    implemented create account feature

--- app/api/requests/users.js
+++ app/api/requests/users.js
@@ -4,3 +4,5 @@ export const createAccount = (data) =>
   axiosInstance.post("/users/create-account/", data, {
     headers: { "Content-Type": "multipart/form-data" },
   });
+
+export const login = (data) => axiosInstance.post("/users/login/", data);
--- components/forms/auth/LoginForm.jsx
+++ components/forms/auth/LoginForm.jsx
@@ -1,18 +1,50 @@
 "use client";
+import { login } from "@/app/api/requests/users";
 import InputField from "@/components/elements/form/InputField";
-import { Input, Link } from "@nextui-org/react";
+import { Button, Input, Link } from "@nextui-org/react";
+import { useForm } from "react-hook-form";
 
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const submitData = async (formData) => {
+    try {
+      const res = await login(formData);
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
-      <form action="#" className="mx-auto mb-0 mt-8 max-w-md space-y-6">
+      <form
+        action="#"
+        className="mx-auto mb-0 mt-8 max-w-md space-y-6"
+        noValidate
+        onSubmit={handleSubmit(submitData)}
+      >
         <InputField
           type="email"
           label="Email Address"
           placeholder="ikoojo@example.com"
+          {...register("email", { required: "Email Address is required" })}
+          isInvalid={!!errors.email}
+          errorMessage={!!errors.email?.message}
         />
 
-        <InputField type="password" label="Password" placeholder="******" />
+        <InputField
+          type="password"
+          label="Password"
+          placeholder="******"
+          {...register("password", { required: "Password is required" })}
+          isInvalid={!!errors.password}
+          errorMessage={!!errors.password?.message}
+        />
         <div className="flex items-center justify-between">
           <p className="text-sm text-gray-500">
             No account?
@@ -20,12 +52,9 @@ const LoginForm = () => {
               Create Account
             </Link>
           </p>
-          <button
-            type="submit"
-            className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
-          >
+          <Button color="primary" isLoading={isSubmitting} type="submit">
             Login
-          </button>
+          </Button>
         </div>
       </form>
     </>
