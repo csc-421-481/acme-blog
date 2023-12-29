931720138179d430b2eb9d134a6f0d6b76ca3c34
jajorgbor <joshuaajogbor@gmail.com>
2023-12-29 20:52:55 +0100

    implemented conditional render for top posts

--- components/sections/TopBlogPosts.jsx
+++ components/sections/TopBlogPosts.jsx
@@ -5,16 +5,18 @@ import AltBlogPost from "../cards/AltBlogPost";
 const TopBlogPosts = () => {
   const { posts } = useGetBlogPosts();
   return (
-    <>
-      <section className="my-8">
-        <div className="flex flex-col md:flex-row gap-6">
-          {posts &&
-            posts
-              .slice(0, 2)
-              .map((each, index) => <AltBlogPost post={each} key={index} />)}
-        </div>
-      </section>
-    </>
+    posts?.length >= 2 && (
+      <>
+        <section className="my-8">
+          <div className="flex flex-col md:flex-row gap-6">
+            {posts &&
+              posts
+                .slice(0, 2)
+                .map((each, index) => <AltBlogPost post={each} key={index} />)}
+          </div>
+        </section>
+      </>
+    )
   );
 };
 export default TopBlogPosts;
