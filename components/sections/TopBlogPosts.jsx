1056dee00db2cdf89ae6ce83028e4b2783413bc1
jajorgbor <joshuaajogbor@gmail.com>
2023-12-29 23:13:02 +0100

    fixed deployment error

--- components/sections/TopBlogPosts.jsx
+++ components/sections/TopBlogPosts.jsx
@@ -4,8 +4,8 @@ import AltBlogPost from "../cards/AltBlogPost";
 
 const TopBlogPosts = () => {
   const { posts } = useGetBlogPosts();
-  return (
-    posts?.length >= 2 && (
+  if (posts?.length >= 2) {
+    return (
       <>
         <section className="my-8">
           <div className="flex flex-col md:flex-row gap-6">
@@ -16,7 +16,7 @@ const TopBlogPosts = () => {
           </div>
         </section>
       </>
-    )
-  );
+    );
+  }
 };
 export default TopBlogPosts;
