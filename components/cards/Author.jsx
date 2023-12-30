ec6fe1eba17a09a180c9ae4e2eaa0fe4a6910d33
jajorgbor <joshuaajogbor@gmail.com>
2023-12-30 14:27:37 +0100

    resolved broken merge

--- components/cards/Author.jsx
+++ components/cards/Author.jsx
@@ -1,6 +1,6 @@
-import Image from 'next/image'
-import { Avatar, Card, CardHeader, CardBody, Link } from '@nextui-org/react'
-import { FileText } from 'react-feather'
+import Image from "next/image";
+import { Avatar, Card, CardHeader, CardBody, Link } from "@nextui-org/react";
+import { FileText } from "react-feather";
 
 const Author = ({ author }) => {
   return (
@@ -22,28 +22,17 @@ const Author = ({ author }) => {
                 </h3>
                 <p className="text-sm">{author.matricNumber}</p>
                 <span className="flex items-center gap-2 text-sm mt-2.5 text-foreground-500">
-                  <FileText size={15} />6 Published posts
+                  <FileText size={15} />
+                  {author.posts_count > 0
+                    ? author.posts_count + " Published posts"
+                    : "No posts yet"}
                 </span>
               </div>
             </div>
-
-            <div>
-              <h3 className="font-semibold text-custom-xl text-dark mb-1">
-                {author.firstName} {author.lastName}
-              </h3>
-              <p className="text-sm">{author.matricNumber}</p>
-              <span className="flex items-center gap-2 text-sm mt-2.5 text-foreground-500">
-                <FileText size={15} />
-                {author.posts_count > 0
-                  ? author.posts_count + " Published posts"
-                  : "No posts yet"}
-              </span>
-            </div>
-          </div>
-        </Card>
-
+          </Card>
+        </Link>
       </>
     )
-  )
-}
-export default Author
+  );
+};
+export default Author;
