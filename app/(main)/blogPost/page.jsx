08009c90f3c8c2428ee4a380ed5d8b2c50d62101
Agbo John <jakeagbo10@gmail.com>
2023-12-30 19:12:47 +0100

    Finished blog post page.

--- app/(main)/blogPost/page.jsx
+++ app/(main)/blogPost/page.jsx
@@ -6,11 +6,8 @@ const page = () => {
     <div className="flex flex-col items-center  my-6">
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-sm text-red-500 font-bold my-2">Travel</h1>
-        <p className="text-xl text-center mt-2 mb-10 md:4/5 lg:w-3/5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
-          harum accusamus, veritatis accusantium fuga quisquam quos sint delectus
-          ullam quidem deleniti reprehenderit earum saepe vel, dolor eius eligendi
-          consectetur odit?
+        <p className="text-5xl font-bold text-center mt-2 mb-10 md:4/5 lg:w-3/5">
+          I moved across the country and never looked back
         </p>
         <div className="flex items-center justify-center mb-10">
           <Avatar
@@ -25,37 +22,174 @@ const page = () => {
 
       </div>
 
-      <div className="h-96 w-full">
-        <img className="w-full h-full" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" />
+      <div className="h-[700px] w-full ">
+        <img className="w-full h-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" />
       </div>
 
-      <p className="text-xs mt-5 mb-5 md:4/5 lg:w-3/5">
+      <p className="text-lg mt-5 mb-5 md:4/5 lg:w-3/5">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
       </p>
 
-      <p className="text-xs mt-5 mb-5 md:4/5 lg:w-3/5">
+      <p className="text-lg mt-5 mb-5 md:4/5 lg:w-3/5">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
       </p>
 
-      <p className="text-xs mt-5 mb-5 md:4/5 lg:w-3/5">
+      <p className="text-lg mt-5 mb-5 md:4/5 lg:w-3/5">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
       </p>
 
-      <p className="text-xs mt-5 mb-5 md:4/5 lg:w-3/5">
+      <p className="text-lg mt-5 mb-5 md:4/5 lg:w-3/5">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
       </p>
 
-      <p className="text-xs mt-5 mb-5 md:4/5 lg:w-3/5">
+      <p className="text-lg mt-5 mb-5 md:4/5 lg:w-3/5">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
       </p>
 
-      <p className="text-xs mt-5 mb-5 md:4/5 lg:w-3/5">
+      <p className="text-lg mt-5 mb-5 md:4/5 lg:w-3/5">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
       </p>
 
-      <p className="text-xs mt-5 mb-5 md:4/5 lg:w-3/5">
+      <p className="text-lg mt-5 mb-5 md:4/5 lg:w-3/5">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
       </p>
+
+      <div className="flex flex-col justify-start my-5  lg:w-3/5 ">
+        <h1 className="text-2xl font-bold text-left">Inspirational welcome</h1>
+        <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+
+      <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+
+      <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+
+      <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+      </div>
+
+      <div className="flex flex-col justify-start my-5  lg:w-3/5 ">
+        <h1 className="text-2xl font-bold text-left">Community work</h1>
+        <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+
+      <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+
+      <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+
+      <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+      </div>
+
+      <div className="flex flex-col justify-start my-5  lg:w-3/5 ">
+        <h1 className="text-2xl font-bold text-left">Longevity</h1>
+        <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+
+      <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+
+      <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+
+      <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+      </div>
+
+      <div className="flex flex-col justify-start my-5  lg:w-3/5 ">
+        <h1 className="text-2xl font-bold text-left">Moral Support</h1>
+        <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+
+      <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+
+      <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+
+      <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+      </div>
+
+      <div className="flex flex-col justify-start my-5  lg:w-3/5 ">
+        <h1 className="text-2xl font-bold text-left">What are the lessons learned</h1>
+        <p className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, magni iusto, harum eligendi numquam voluptatem ab magnam blanditiis perspiciatis veniam sapiente facilis dolor ea voluptas quas dignissimos doloribus tempora praesentium!
+      </p>
+
+      <li className="text-lg mt-5 mb-5 md:4/5">
+        Lorem ilisum dolor sit amet consectetur adiliisicing elit. Maxime, magni iusto, harum eligendi numquam volulitatem ab magnam blanditiis liersliiciatis veniam saliiente facilis dolor ea volulitas quas dignissimos doloribus temliora liraesentium!
+      </li>
+
+      <li className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ilisum dolor sit amet consectetur adiliisicing elit. Maxime, magni iusto, harum eligendi numquam volulitatem ab magnam blanditiis liersliiciatis veniam saliiente facilis dolor ea volulitas quas dignissimos doloribus temliora liraesentium!
+      </li>
+
+      <li className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ilisum dolor sit amet consectetur adiliisicing elit. Maxime, magni iusto, harum eligendi numquam volulitatem ab magnam blanditiis liersliiciatis veniam saliiente facilis dolor ea volulitas quas dignissimos doloribus temliora liraesentium!
+      </li>
+      
+      <li className="text-lg mt-5 mb-5 md:4/5 ">
+        Lorem ilisum dolor sit amet consectetur adiliisicing elit. Maxime, magni iusto, harum eligendi numquam volulitatem ab magnam blanditiis liersliiciatis veniam saliiente facilis dolor ea volulitas quas dignissimos doloribus temliora liraesentium!
+      </li>
+
+      </div>
+
+      <h1 className="text-center text-blue-700 mb-10">View all posts</h1>
+
+  
+  
+  <div className="relative inline-flex items-center bg-gray-50 rounded-lg">
+  
+    <div className="flex flex-col relative overflow-hidden height-auto text-foreground p-5">
+      <div className="flex flex-wrap items-center gap-8">
+        <div className="w-24 h-24 rounded-full overflow-hidden">
+          <span className="flex relative justify-center items-center bg-default text-default-foreground rounded-full w-24 h-24">
+            <span
+              aria-label="profile image"
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center text-inherit w-full h-full"
+              role="img"
+            >
+              { <Avatar
+            src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+            className="w-full h-full"
+          />}
+            </span>
+          </span>
+        </div>
+        <div>
+          <h3 className="font-semibold text-custom-xl text-dark mb-1">About agbo jake</h3>
+          <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in quas magni fuga praesentium dolorum?</p>
+          <span className="flex items-center gap-2 text-sm mt-2.5 text-foreground-500">
+            
+          <h1 className="text-center text-blue-700 mb-10">View Profile</h1>
+          </span>
+        </div>
+      </div>
+    </div>
+  
+</div>
+
+
 
 
     </div>
